80d72a6c33f78fa9d472fd64e21fb6522368c81a
colemank <colemank@allegheny.edu>
2021-09-22 19:38:17 -0400

    Continue adding starter management methods

--- BusinessLogic.cs
+++ BusinessLogic.cs
@@ -8,9 +8,11 @@ namespace LibWorkInstructions {
 
     #region database-mocking
     public class MockDB {
-      public ListJob Jobs { get; set; }
+      public List<Job> Jobs { get; set; }
+      public List<OpSpec> opSpecs { get; set; }
+      public List<QualityClause> qualityClauses { get; set; }
     }
-    private MockDB db;  //this should contain anyall state used in this BusinessLogic class.
+    private MockDB db;  //this should contain any all state used in this BusinessLogic class.
     public BusinessLogic() {
       this.db = new MockDB {
         Jobs = new ListJob()
@@ -27,12 +29,7 @@ namespace LibWorkInstructions {
     public Op GetOp(int opId) =
       db.Jobs.SelectMany(y = y.Ops).First(y = y.Id == opId);
 
-    public void addJob(Job newJob)
-        {
-            db.Jobs.Add(newJob);
-        }
-
-    public int findIndex(string jobId)
+    public int findJobIndex(string jobId)
         {
             int count = 0, index = -1;
             foreach (Job j in db.Jobs)
@@ -44,17 +41,53 @@ namespace LibWorkInstructions {
             return index;
         }
 
-    public void changeJob(string jobId, Job newJob)
+    public int findSpecIndex(string name)
         {
-            db.Jobs[findIndex(jobId)] = newJob;
+            int count = 0, index = -1;
+            foreach (OpSpec s in db.opSpecs)
+            {
+                if(s.Name == name)
+                    index = count;
+                count++;
+            }
+            return index
+        }
+
+    public void addJob(Job newJob)
+        {
+            db.Jobs.Add(newJob);
+        }
+
+    public void changeJob(Job job, Job newJob)
+        {
+            db.Jobs[findJobIndex(job.Id)] = newJob;
         }
 
     public void removeJob(string jobId)
         {
-            db.Jobs.RemoveAt(findIndex(jobId));
+            db.Jobs.RemoveAt(findJobIndex(jobId));
         }
 
-    public void 
+     public void addSpec(OpSpec spec)
+        {
+            db.opSpecs.Add(spec);
+        }
+
+    public void changeSpec(OpSpec spec, OpSpec newSpec)
+        {
+            db.opSpecs[findSpecIndex(spec.Name)] = newSpec;
+        }
+
+    public void removeSpec(string specName)
+        {
+            db.opSpecs.RemoveAt(findSpecIndex(specName));
+        }
+
+    public void addQualityClause(QualityClause clause)
+        {
+            db.qualityClauses.Add(clause);
+        }
+
     static void Main(string[] args)
         {
            
