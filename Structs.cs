5e4c007ca20330bf1d4428cf78a49f7eb07dbe04
Wesley Long <46755903+WesleyL30@users.noreply.github.com>
2021-09-23 09:19:20 -0400

    Add work instructions class

--- Structs.cs
+++ Structs.cs
@@ -6,10 +6,6 @@ namespace LibWorkInstructions {
     
     // There's a list of operation specifications for a job that can be applied to a list of operations
     public List<OpSpec> Specs { get; set; }
-    // There's a list of images for a particular work instruction
-    public List<string> Images { get; set; }
-    // Work instructions have an approval status
-    public bool Approved { get; set; }
       
     // Work instructions have rich text content that 
     public string TextContent { get; set; }
@@ -40,39 +36,53 @@ namespace LibWorkInstructions {
       
     }
 
-    // TODO: Add work instructions class
+    // Work instructions are specific to a particular revision of an operation
+    public class WorkInstruction {
+        // Identifier for work instructions
+        public string Id { get; set; }
+        // There's a list of images for a particular work instruction
+        public List<string> Images { get; set; }
+        // Work instructions have an approval status
+        public bool Approved { get; set; }
+        // Work instructions can have one or more revisions
+        public List<Revision> WorkRevs { get; set;}
+        // Placeholder for rich content
+        public List<string> HtmlBlob { get; set;}
+        
+        }
     // TODO: Possibly add description of classes
 
     public class OpSpec {
-            public string Name { get; set; }
-            public Revision Rev { get; set; }
-            public string Notice { get; set;  }
-            public string Class { get; set; }
-            public string Type { get; set; }
-            public string Method { get; set; }
-            public string Grade { get; set; }
-            public string Level { get; set; }
-            public string Proctype { get; set; }
-            public string Servicecond { get; set; }
-            public string Status { get; set; }
-            public string Comment { get; set; }
-            public List<Revision> SpecRevs { get; set; }
+        public string Name { get; set; }
+        public Revision Rev { get; set; }
+        public string Notice { get; set;  }
+        public string Class { get; set; }
+        public string Type { get; set; }
+        public string Method { get; set; }
+        public string Grade { get; set; }
+        public string Level { get; set; }
+        public string Proctype { get; set; }
+        public string Servicecond { get; set; }
+        public string Status { get; set; }
+        public string Comment { get; set; }
+        public List<Revision> SpecRevs { get; set; }
 
         }
     
     public class QualityClause
         {
-            public string Clause { get; set; }
+        public string Clause { get; set; }
 
-            public Revision JobRev { get; set; }
+        public Revision JobRev { get; set; }
 
-            public List<Revision> ClauseRevs { get; set; }
+        public List<Revision> ClauseRevs { get; set; }
         }
 
     public class Revision
         {
-            public string Category { get; set; }
-            public string Version { get; set; }
+        public string Category { get; set; }
+        public string Version { get; set; }
+        public WorkInstruction? Instruction { get; set; }
         }
     }
 }
