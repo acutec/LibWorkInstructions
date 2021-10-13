c0f38fed7ad77d426c79d9382feab51ab24da725
colemank <colemank@allegheny.edu>
2021-10-13 09:22:15 -0400

    Complete audit log

--- LibWorkInstructions/LibWorkInstructions/BusinessLogic.cs
+++ LibWorkInstructions/LibWorkInstructions/BusinessLogic.cs
@@ -37,11 +37,14 @@ namespace LibWorkInstructions
             db.Jobs.Add(newJob.Id, newJob);
 
             var args = new Dictionary<string, string>();
+            var jobDict = new Dictionary<string, Job>();
             args["Job"] = newJob.Id;
+            jobDict[args["Job"]] = newJob;
             db.AuditLog.Add(new Event
             {
                 Action = "AddJob",
                 Args = args,
+                newJob = jobDict,
                 When = DateTime.Now,
             });
         }
@@ -54,11 +57,14 @@ namespace LibWorkInstructions
             db.WorkInstructions.Add(newWorkInstruction.Id, newWorkInstruction);
 
             var args = new Dictionary<string, string>();
+            var workIDict = new Dictionary<string, WorkInstruction>();
             args["WorkInstruction"] = newWorkInstruction.Id.ToString();
+            workIDict[args["WorkInstruction"]] = newWorkInstruction;
             db.AuditLog.Add(new Event
             {
                 Action = "AddWorkInstruction",
                 Args = args,
+                newWorkI = workIDict,
                 When = DateTime.Now,
             });
         }
@@ -153,14 +159,14 @@ namespace LibWorkInstructions
             });
         }
 
-        public void CloneWorkInstruction(int workId, string targetJobId)
+        public void CloneWorkInstruction(int sourceWorkId, string targetJobId)
         {
             List<int> duplicate = new List<int>();
             foreach (List<List<int>> list in db.JobRefToWorkInstructionRefs.Values)
             {
                 foreach (List<int> workInstruction in list)
                 {
-                    if (workInstruction.Contains(workId))
+                    if (workInstruction.Contains(sourceWorkId))
                         duplicate = workInstruction;
                 }
             }
@@ -168,7 +174,7 @@ namespace LibWorkInstructions
             db.JobRefToWorkInstructionRefs[targetJobId].Add(duplicate);
 
             var args = new Dictionary<string, string>();
-            args["WorkInstruction"] = workId.ToString();
+            args["SourceWorkInstruction"] = sourceWorkId.ToString();
             args["TargetJob"] = targetJobId;
             db.AuditLog.Add(new Event
             {
@@ -181,6 +187,18 @@ namespace LibWorkInstructions
         public void AddSpec(OpSpec opSpec)
         {
             db.OpSpecs.Add(opSpec.Id, opSpec);
+
+            var args = new Dictionary<string, string>();
+            var specDict = new Dictionary<string, OpSpec>();
+            args["OpSpec"] = opSpec.Id.ToString();
+            specDict[args["OpSpec"]] = opSpec;
+            db.AuditLog.Add(new Event
+            {
+                Action = "AddSpec",
+                Args = args,
+                newOpSpec = specDict,
+                When = DateTime.Now,
+            });
         }
 
         public void ChangeSpec(int oldSpecId, OpSpec newOpSpec)
@@ -191,11 +209,30 @@ namespace LibWorkInstructions
                                                                 select workInstruction).ToList();
             foreach(WorkInstruction workInstruction in invalidateWorkInstructions)
                 workInstruction.Approved = false;
+
+            var args = new Dictionary<string, string>();
+            args["OldSpec"] = oldSpecId.ToString();
+            args["NewSpec"] = newOpSpec.Id.ToString();
+            db.AuditLog.Add(new Event
+            {
+                Action = "ChangeSpec",
+                Args = args,
+                When = DateTime.Now,
+            });
         }
 
         public void DeleteSpec(int specId)
         {
             db.OpSpecs.Remove(specId);
+
+            var args = new Dictionary<string, string>();
+            args["DeletedSpec"] = specId.ToString();
+            db.AuditLog.Add(new Event
+            {
+                Action = "DeleteSpec",
+                Args = args,
+                When = DateTime.Now,
+            });
         }
 
         public void MergeSpecs(int workId1, int workId2)
@@ -203,36 +240,96 @@ namespace LibWorkInstructions
             db.WorkInstructions[workId1].OpSpecs =
                 Enumerable.ToList(db.WorkInstructions[workId1].OpSpecs.Union(db.WorkInstructions[workId2].OpSpecs));
             db.WorkInstructions[workId2].OpSpecs = db.WorkInstructions[workId1].OpSpecs;
+
+            var args = new Dictionary<string, string>();
+            args["WorkInstruction1"] = workId1.ToString();
+            args["WorkInstruction2"] = workId2.ToString();
+            db.AuditLog.Add(new Event
+            {
+                Action = "MergeSpecs",
+                Args = args,
+                When = DateTime.Now,
+            });
         }
 
         public void SplitSpecs(int workId1, int workId2)
         {
             db.WorkInstructions[workId2].OpSpecs = db.WorkInstructions[workId1].OpSpecs;
+
+            var args = new Dictionary<string, string>();
+            args["WorkInstruction1"] = workId1.ToString();
+            args["WorkInstruction2"] = workId2.ToString();
+            db.AuditLog.Add(new Event
+            {
+                Action = "SplitSpecs",
+                Args = args,
+                When = DateTime.Now,
+            });
         }
 
-        public void CloneSpecs(int workId, int newWorkId)
+        public void CloneSpecs(int sourceWorkId, int targetWorkId)
         {
-            WorkInstruction newInstruction = new WorkInstruction();
-            newInstruction.Id = newWorkId;
-            newInstruction.OpSpecs = db.WorkInstructions[workId].OpSpecs;
-            db.WorkInstructions.Add(newInstruction.Id, newInstruction);
+            db.WorkInstructions[targetWorkId].OpSpecs =
+                Enumerable.ToList(db.WorkInstructions[sourceWorkId].OpSpecs.Union(db.WorkInstructions[targetWorkId].OpSpecs));
+
+            var args = new Dictionary<string, string>();
+            args["SourceWorkId"] = sourceWorkId.ToString();
+            args["TargetWorkId"] = targetWorkId.ToString();
+            db.AuditLog.Add(new Event
+            {
+                Action = "CloneSpecs",
+                Args = args,
+                When = DateTime.Now,
+            });
         }
 
         public void CreateQualityClause(QualityClause qualityClause)
         {
             db.QualityClauses.Add(qualityClause.Id, qualityClause);
+
+            var args = new Dictionary<string, string>();
+            var clauseDict = new Dictionary<string, QualityClause>();
+            args["QualityClause"] = qualityClause.Id.ToString();
+            clauseDict[args["QualityClause"]] = qualityClause;
+            db.AuditLog.Add(new Event
+            {
+                Action = "CreateQualityClause",
+                Args = args,
+                newQualityC = clauseDict,
+                When = DateTime.Now,
+            });
         }
 
         public void MergeQualityClauses(string job1, string job2)
         {
             db.JobRefToQualityClauseRefs[job1] =
-                Enumerable.ToList(db.JobRefToQualityClauseRefs[job1].Union(db.JobRefToQualityClauseRefs[job2]));
+                db.JobRefToQualityClauseRefs[job1].Union(db.JobRefToQualityClauseRefs[job2]).ToList();
             db.JobRefToQualityClauseRefs[job2] = db.JobRefToQualityClauseRefs[job1];
+
+            var args = new Dictionary<string, string>();
+            args["Job1"] = job1;
+            args["Job2"] = job2;
+            db.AuditLog.Add(new Event
+            {
+                Action = "MergeQualityClauses",
+                Args = args,
+                When = DateTime.Now,
+            });
         }
 
-        public void SplitQualityClauses(string job1, string job2)
+        public void SplitQualityClauses(string sourceJob, string targetJob)
         {
-            db.JobRefToQualityClauseRefs[job2] = db.JobRefToQualityClauseRefs[job1];
+            db.JobRefToQualityClauseRefs[targetJob] = db.JobRefToQualityClauseRefs[sourceJob];
+
+            var args = new Dictionary<string, string>();
+            args["SourceJob"] = sourceJob;
+            args["TargetJob"] = targetJob;
+            db.AuditLog.Add(new Event
+            {
+                Action = "SplitQualityClauses",
+                Args = args,
+                When = DateTime.Now,
+            });
         }
 
         public void CloneQualityClauses(string sourceJobId, string targetJobId)
@@ -241,6 +338,16 @@ namespace LibWorkInstructions
                 .AddRange(db.JobRefToQualityClauseRefs[sourceJobId]);
             db.JobRefToQualityClauseRefs[targetJobId] =
                     db.JobRefToQualityClauseRefs[targetJobId].Distinct().ToList();
+
+            var args = new Dictionary<string, string>();
+            args["SourceJob"] = sourceJobId;
+            args["TargetJob"] = targetJobId;
+            db.AuditLog.Add(new Event
+            {
+                Action = "CloneQualityClauses",
+                Args = args,
+                When = DateTime.Now,
+            });
         }
 
         public void DisplayPriorRevisionsOfWorkInstruction(string job, int latestWorkId)
